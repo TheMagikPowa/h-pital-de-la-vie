ae906e256ad00ec3272a65f76a7806142da0a28a
Alessandro <alessandro.pelosi96@gmail.com>
2026-04-20 21:19:27 +0200

    pw changing

--- config.py
+++ config.py
@@ -1,10 +1,4 @@
-JWT_KEY="a9F$kL92!dP0xQw7ZsM1vB3rT8yH4nC"               #for jwt
-JWT_ALG="HS256"
-
-
 DB_HOST='localhost'                         #for db
 DB_USER='root'
 DB_PW='Nicola@96'
 DB_NAME='hopital_de_la_vie'
-
-
